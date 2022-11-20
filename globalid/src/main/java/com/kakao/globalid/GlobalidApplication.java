87a0cddf7783951138042af39ad6be0164b0f240
LEEKW <LEEKW@DESKTOP-G3IUI2U>
2022-11-20 11:37:55 +0900

    globalid master

---
+++ globalid/src/main/java/com/kakao/globalid/GlobalidApplication.java
@@ -0,0 +1,15 @@
+package com.kakao.globalid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalidApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalidApplication.class, args);
+	}
+
+	
+
+}
